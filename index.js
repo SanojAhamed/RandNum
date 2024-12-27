fe4e96003f6196ebfea59f5818ba6b73793bc047
Sanoj Ahamed <sanojahamed1730@gmail.com>
2024-12-27 11:11:36 +0530

    feat: package update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const rendomNumberGenerator = (min=0,max=100)=> {
-   return Math.round(math.rondom() * (max-min)-min);
+function randomNumber(min = 0, max = 1, isInteger = false) {
+    const number = Math.random() * (max - min) + min;
+    return isInteger ? Math.floor(number) : number;
 }
 
-module.exports = rendomNumberGenerator;
+module.exports = randomNumber;
